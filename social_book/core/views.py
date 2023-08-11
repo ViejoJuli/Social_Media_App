2880e8b3bbec8b5cd466f58f172d07c38a503372
Julian Rios <jdrios111@gmail.com>
2023-08-11 05:37:59 -0500

    Static implementation

--- social_book/core/views.py
+++ social_book/core/views.py
@@ -2,5 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
+
+
 def index(request):
-    return HttpResponse('<h1> Welcome to MusicYas</h1>')
+    return render(request, 'index.html')
