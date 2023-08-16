6fd7428bd35db2b1931833abe571002ba0db6b03
Julian Rios <jdrios111@gmail.com>
2023-08-15 22:18:53 -0500

    Login and Singout

--- social_book/core/views.py
+++ social_book/core/views.py
@@ -2,11 +2,13 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.models import User, auth
 from django.contrib import messages
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 from .models import Profile
+
 
 # Create your views here.
 
-
+@login_required(login_url='signin')  # Requieres login
 def index(request):
     return render(request, 'index.html')
 
@@ -46,3 +48,27 @@ def signup(request):
 
     else:
         return render(request, 'signup.html')
+
+
+def signin(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(username=username, password=password)
+
+        if user is not None:
+            auth.login(request, user)
+            return redirect('/')
+        else:
+            messages.info(request, 'Invalid username or password')
+            return redirect('signin')
+
+    else:
+        return render(request, 'signin.html')
+
+
+@login_required(login_url='signin')  # Requieres login
+def logout(request):
+    auth.logout(request)
+    return redirect('signin')
