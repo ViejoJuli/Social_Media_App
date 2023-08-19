3ebc59f379a7047ab962e768647a990813371f2d
Julian Rios <jdrios111@gmail.com>
2023-08-19 07:44:22 -0500

    Post Feed

--- social_book/core/views.py
+++ social_book/core/views.py
@@ -13,7 +13,8 @@ def index(request):
     user_object = User.objects.get(username=request.user.username)
     user_profile = Profile.objects.get(user=user_object)
     # Send user profile into html
-    return render(request, 'index.html', {'user_profile': user_profile})
+    posts = Post.objects.all()
+    return render(request, 'index.html', {'user_profile': user_profile, 'posts': posts})
 
 
 def signup(request):
